5e7b944ebe749962ebf39642eff8a1f4dd0ee80d
Chris Quin <chrisq@ingenuity.kiwi>
2020-02-23 09:59:26 +1300

    Use onchange to set const json value.

--- pages/feedback.js
+++ pages/feedback.js
@@ -1,38 +1,34 @@
 import Head from "next/head";
 import Page from "../components/Page";
 import Link from "next/link";
-import { makeStyles } from '@material-ui/core/styles';
-import TextField from '@material-ui/core/TextField';
-import styled from 'styled-components'
+import { makeStyles } from "@material-ui/core/styles";
+import TextField from "@material-ui/core/TextField";
+import styled from "styled-components";
 
 const CoolButton = styled.button`
-
-margin-top: 1rem;
-  border-radius:120px;
+  margin-top: 1rem;
+  border-radius: 120px;
   width: 14rem;
   height: 3rem;
   font-size: 1.2rem;
   border: none;
   background-color: black;
   color: white;
-  font-family: 'Lexend Deca';
-
-`
-
+  font-family: "Lexend Deca";
+`;
 
 const useStyles = makeStyles(theme => ({
   root: {
-    '& .MuiTextField-root': {
+    "& .MuiTextField-root": {
       margin: theme.spacing(1),
       width: 200,
-      lineHeight: 1.5,
-    },
-  },
+      lineHeight: 1.5
+    }
+  }
 }));
 
-
-var submitInput;
 var outputText;
+const Data = { submitValue: "" };
 
 function getQueryStringValue(key) {
   return decodeURIComponent(
@@ -50,12 +46,7 @@ function getQueryStringValue(key) {
 
 const handleClick = (dept, branch) => {
   var enteredText = "";
-
-  if (!submitInput) {
-    alert("element not found");
-  }
-
-  enteredText = submitInput.value;
+  enteredText = Data.submitValue;
 
   let data = {
     dept: dept,
@@ -85,200 +76,175 @@ const handleClick = (dept, branch) => {
 
 export default function Feedback() {
   const classes = useStyles();
-  const [value, setValue] = React.useState('Controlled');
+  const [value, setValue] = React.useState("Controlled");
 
   const handleChange = event => {
     setValue(event.target.value);
   };
 
   return (
-  <Page>
-    <Head>
-      <title>Feedback</title>
-      <p> This will get analysed by</p>
-      <link rel="icon" href="/favicon.ico" />
-    </Head>
-    <main>
-      <h2>Feedback</h2>
-      <form className="feedback-form">
-      <TextField
-          id="outlined-multiline-static"
-          label="Leave your feedback here."
-          multiline
-          fullWidth
-          rows="4"
-    
-          variant="outlined"
-          name="message"
-          ref={input => (submitInput = input)}
-        />
-       
-      </form>
-      <CoolButton
-        className="feedback-submit"
-        name="submit-button"
-        onClick={e =>
-          handleClick(
-            getQueryStringValue("dept"),
-            getQueryStringValue("branch")
-          )
+    <Page>
+      <Head>
+        <title>Feedback</title>
+        <p> This will get analysed by</p>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main>
+        <h2>Feedback</h2>
+        <form className="feedback-form">
+          <TextField
+            id="outlined-multiline-static"
+            label="Leave your feedback here."
+            multiline
+            fullWidth
+            rows="4"
+            variant="outlined"
+            name="message"
+            onChange={e => (Data.submitValue = e.target.value)}
+          />
+        </form>
+        <CoolButton
+          className="feedback-submit"
+          name="submit-button"
+          onClick={e =>
+            handleClick(
+              getQueryStringValue("dept"),
+              getQueryStringValue("branch")
+            )
+          }
+        >
+          Submit
+        </CoolButton>
+
+        <div className="test">
+          <p>Result:</p>
+          <p>
+            <textarea name="result" ref={d => (outputText = d)} />
+          </p>
+        </div>
+      </main>
+      <footer>Powered by Loop</footer>
+
+      <style jsx>{`
+        .feedback-form {
+          margin: 0 auto;
+          width: 20rem;
         }
-      >
-        Submit
-      </CoolButton>
-
-      <div className="test">
-        <p>Result:</p>
-        <p>
-          <textarea name="result" ref={d => (outputText = d)} />
-        </p>
-      </div>
-    </main>
-    <footer>Powered by Loop</footer>
-
-    <style jsx>{`
-      .feedback-form {
-        margin: 0 auto;
-        width: 20rem;
-      }
-
-      .feedback-text {
-        width: 100%;
-      }
-
-      .feedback-submit {
-      }
-
-      .test {
-        display: none;
-      }
-
-      .container {
-        min-height: 100vh;
-        padding: 0 0.5rem;
-        display: flex;
-        flex-direction: column;
-        justify-content: center;
-        align-items: center;
-      }
-
-      main {
-        padding: 1rem 0;
-        flex: 1;
-        display: flex;
-        flex-direction: column;
-        justify-content: center;
-        align-items: center;
-      }
-
-      footer img {
-        margin-left: 0.5rem;
-      }
-
-      footer a {
-        display: flex;
-        justify-content: center;
-        align-items: center;
-      }
-
-      a {
-        color: inherit;
-        text-decoration: none;
-      }
-
-      .title a {
-        color: #0070f3;
-        text-decoration: none;
-   
-      }
-
-      .title a:hover,
-      .title a:focus,
-      .title a:active {
-        text-decoration: underline;
-      }
-
-      .title {
-        margin: 0;
-        line-height: 1.15;
-        font-size: 4rem;
-        text-align: left;
-       
-      }
-
-      .title,
-      .description {
-        text-align: center;
-      }
-
-      .description {
-        line-height: 1.5;
-        font-size: 1.5rem;
-      }
-
-      code {
-        background: #fafafa;
-        border-radius: 5px;
-        padding: 0.75rem;
-        font-size: 1.1rem;
-        font-family: Menlo, Monaco, Lucida Console, Liberation Mono,
-          DejaVu Sans Mono, Bitstream Vera Sans Mono, Courier New, monospace;
-      }
-
-      .grid {
-        display: flex;
-        align-items: center;
-        justify-content: center;
-        flex-wrap: wrap;
-
-        max-width: 800px;
-        margin-top: 3rem;
-      }
-
-      .card {
-        margin: 1rem;
-        flex-basis: 45%;
-        padding: 1.5rem;
-        text-align: left;
-        color: inherit;
-        text-decoration: none;
-        border: 1px solid #eaeaea;
-        border-radius: 10px;
-        transition: color 0.15s ease, border-color 0.15s ease;
-      }
-
-      .card:hover,
-      .card:focus,
-      .card:active {
-        color: #0070f3;
-        border-color: #0070f3;
-      }
-
-      .card h3 {
-        margin: 0 0 1rem 0;
-        font-size: 1.5rem;
-      }
-
-      .card p {
-        margin: 0;
-        font-size: 1.25rem;
-        line-height: 1.5;
-      }
-      footer {
-  position: absolute;
-  bottom: 1rem;
-  left: 1rem;
-  }
-
-
-      @media (max-width: 600px) {
-        .grid {
+        .feedback-text {
           width: 100%;
-          flex-direction: column;
         }
-      }
-    `}</style>
-  </Page>)
+        .feedback-submit {
+        }
+        .test {
+          display: none;
+        }
+        .container {
+          min-height: 100vh;
+          padding: 0 0.5rem;
+          display: flex;
+          flex-direction: column;
+          justify-content: center;
+          align-items: center;
+        }
+        main {
+          padding: 1rem 0;
+          flex: 1;
+          display: flex;
+          flex-direction: column;
+          justify-content: center;
+          align-items: center;
+        }
+        footer img {
+          margin-left: 0.5rem;
+        }
+        footer a {
+          display: flex;
+          justify-content: center;
+          align-items: center;
+        }
+        a {
+          color: inherit;
+          text-decoration: none;
+        }
+        .title a {
+          color: #0070f3;
+          text-decoration: none;
+        }
+        .title a:hover,
+        .title a:focus,
+        .title a:active {
+          text-decoration: underline;
+        }
+        .title {
+          margin: 0;
+          line-height: 1.15;
+          font-size: 4rem;
+          text-align: left;
+        }
+        .title,
+        .description {
+          text-align: center;
+        }
+        .description {
+          line-height: 1.5;
+          font-size: 1.5rem;
+        }
+        code {
+          background: #fafafa;
+          border-radius: 5px;
+          padding: 0.75rem;
+          font-size: 1.1rem;
+          font-family: Menlo, Monaco, Lucida Console, Liberation Mono,
+            DejaVu Sans Mono, Bitstream Vera Sans Mono, Courier New, monospace;
+        }
+        .grid {
+          display: flex;
+          align-items: center;
+          justify-content: center;
+          flex-wrap: wrap;
+          max-width: 800px;
+          margin-top: 3rem;
+        }
+        .card {
+          margin: 1rem;
+          flex-basis: 45%;
+          padding: 1.5rem;
+          text-align: left;
+          color: inherit;
+          text-decoration: none;
+          border: 1px solid #eaeaea;
+          border-radius: 10px;
+          transition: color 0.15s ease, border-color 0.15s ease;
+        }
+        .card:hover,
+        .card:focus,
+        .card:active {
+          color: #0070f3;
+          border-color: #0070f3;
+        }
+        .card h3 {
+          margin: 0 0 1rem 0;
+          font-size: 1.5rem;
+        }
+        .card p {
+          margin: 0;
+          font-size: 1.25rem;
+          line-height: 1.5;
+        }
+        footer {
+          position: absolute;
+          bottom: 1rem;
+          left: 1rem;
+        }
+        @media (max-width: 600px) {
+          .grid {
+            width: 100%;
+            flex-direction: column;
+          }
+        }
+      `}</style>
+    </Page>
+  );
 }
 
 /*
